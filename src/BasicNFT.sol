3039dcbe1d170257af20a604c18b58cfea8bdaa5
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-06 16:59:50 -0500

    migrate to foundry

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { ERC721 } from '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
     uint256 private tokenCounter;
     mapping(uint256 => string) private tokenIdToUri;
 
-    constructor() ERC721('Golems', 'GOL') {
+    constructor() ERC721("Golems", "GOL") {
         tokenCounter = 0;
     }
 
@@ -17,7 +17,9 @@ contract BasicNFT is ERC721 {
         _safeMint(msg.sender, tokenCounter);
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
         // return 'ipfs://QmZBWXvTxruyW8qCKgiWLkPzyeGhKiYSayYmt7MvNtmuDr';
         return tokenIdToUri[tokenId];
     }
