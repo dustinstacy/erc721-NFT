b9ee077a720834aca2dc7a0e2a0461502a8b9198
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-07 12:06:11 -0500

    fix tokenId bug

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -1,25 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { ERC721 } from '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
 contract BasicNFT is ERC721 {
     uint256 private tokenCounter;
     mapping(uint256 => string) private tokenIdToUri;
 
-    constructor() ERC721("Golems", "GOL") {
+    constructor() ERC721('Golems', 'GOL') {
         tokenCounter = 0;
     }
 
     function mintNFT(string memory tokenUri) public {
         tokenIdToUri[tokenCounter] = tokenUri;
-        tokenCounter++;
         _safeMint(msg.sender, tokenCounter);
+        tokenCounter++;
     }
 
-    function tokenURI(
-        uint256 tokenId
-    ) public view override returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
         // return 'ipfs://QmZBWXvTxruyW8qCKgiWLkPzyeGhKiYSayYmt7MvNtmuDr';
         return tokenIdToUri[tokenId];
     }
