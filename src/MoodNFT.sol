4bd76e6cae3c743344d3fdba9164bfcda58b2e14
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-07 13:15:27 -0500

    start tokenURI function

---
+++ src/MoodNFT.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { ERC721 } from '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import { Base64 } from '@openzeppelin/contracts/utils/Base64.sol';
+
+contract MoodNFT is ERC721 {
+    uint256 private tokenCounter;
+    string private sadSVGImageURI;
+    string private happySVGImageURI;
+
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+
+    mapping(uint256 tokenId => Mood) private tokenIdToMood;
+
+    constructor(string memory _sadSVGImageURI, string memory _happySVGImageURI) ERC721('Mood', 'MD') {
+        tokenCounter = 0;
+        sadSVGImageURI = _sadSVGImageURI;
+        happySVGImageURI = _happySVGImageURI;
+    }
+
+    function mintNFT() public {
+        _safeMint(msg.sender, tokenCounter);
+        tokenIdToMood[tokenCounter] = Mood.HAPPY;
+        tokenCounter++;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        string memory imageURI;
+        if (tokenIdToMood[tokenId] == Mood.HAPPY) {
+            imageURI = happySVGImageURI;
+        } else {
+            imageURI = sadSVGImageURI;
+        }
+
+        string memory tokenMetadata = string.concat(
+            '{"name":"',
+            name(),
+            '", "description": "An NFT that reflects the owners mood.", "attributes": [{"trait_type": "moodiness", "value": 100}]"image": "',
+            imageURI,
+            '"}'
+        );
+    }
+}
